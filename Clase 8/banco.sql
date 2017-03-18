edd0a54d1c17ed55619a46ae5a9a677e8f7ae3a7
Efren Jimenez <efjimenez@itcr.ac.cr>
2017-03-17 18:07:07 -0600

    clase 8

---
+++ Clase 8/banco.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE db_banco;
+
+
+CREATE TABLE cuenta_corriente(id_cliente serial NOT NULL,
+                                                nombre_cliente varchar(30) NOT NULL,
+                                                                           monto numeric NOT NULL,
+                                                                                         CONSTRAINT pk_cuenta_corriente PRIMARY KEY (id_cliente));
+
+
+CREATE TABLE cuenta_ahorros(id_cliente serial NOT NULL,
+                                              nombre_cliente varchar(30) NOT NULL,
+                                                                         monto numeric NOT NULL,
+                                                                                       CONSTRAINT pk_cuenta_ahorros PRIMARY KEY (id_cliente));
+
+
+CREATE TABLE bitacora(id_bitacora serial NOT NULL,
+                                         tiempo TIME WITH TIME ZONE,
+                                                               descripcion varchar(150),
+                                                                           CONSTRAINT pk_log PRIMARY KEY (id_bitacora));
+
+
+
